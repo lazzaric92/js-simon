cbb43b90befe4b8e440b2057c91caa8a994d0a7d
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-21 18:43:51 +0200

    added functions to ask for numbers and compare arrays

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 const randomNumbersArray = [];
 let timerTime = 30;
-const showNumbers = randomNumbersArray.join(' - ');
-randomIntNumbersGenerator(5, 0, 100, randomNumbersArray);
+const userNumbersArray = [];
+const correctUserNumbers = [];
+const wrongUserNumbers = [];
+getRandomIntNumbersGenerator(5, 0, 100, randomNumbersArray);
 
-setTimeout(showNumbers, 5000);
-setTimeout(timerFunction, 6000, timerTime);
+setTimeout(function(){
+    console.log(randomNumbersArray.join(' - '));
+    timerFunction(timerTime);
+}, 4000);
+
+setTimeout(function(){
+    getUserNumbers(5, userNumbersArray);
+    console.log(userNumbersArray);
+    compareArrays(userNumbersArray, randomNumbersArray, correctUserNumbers, wrongUserNumbers);
+
+}, (timerTime * 1000 + 7000))
+
 
 
 
@@ -16,7 +28,7 @@ setTimeout(timerFunction, 6000, timerTime);
  * @param {*} max Math.random interval max value, included
  * @param {*} randomIntNumbersArray Container array
  */
-function randomIntNumbersGenerator(numbersToGenerate, min, max, randomIntNumbersArray){
+function getRandomIntNumbersGenerator(numbersToGenerate, min, max, randomIntNumbersArray){
     let index = 1;
     while (randomIntNumbersArray.length < numbersToGenerate){
         const uniqueRandomNumber = Math.floor(Math.random() * (max - min + 1) + min);
@@ -38,3 +50,35 @@ function timerFunction(seconds){
     }, 1000)
 }
 
+// --> function to ask the user for numbers
+/**
+ * Function to ask the user for numbers via prompt
+ * @param {*} numbersToAsk How many numbers the user must type
+ * @param {*} numbersArray Container array
+ */
+function getUserNumbers(numbersToAsk, numbersArray){
+    let index = 0;
+    while (index < numbersToAsk){
+        let userNumber = Number.parseInt(prompt('Type a number') ,10);
+        while (userNumber < 0 || userNumber > 100 || Number.isNaN(userNumber)){
+            userNumber = Number.parseInt(prompt('Type a number') ,10);
+        }
+        numbersArray.push(userNumber);
+        index++;
+    }
+}
+
+// --> function to compare arrays
+function compareArrays(arrayA, arrayB, correctNumbersArray, wrongNumbersArray){
+    for(let index = 0; index < arrayA.length; index++){
+        if(arrayB.includes(arrayA[index])){
+            correctNumbersArray.push(arrayA[index]);
+        } else {
+            wrongNumbersArray.push(arrayA[index]);
+        }
+    }
+
+    console.log(correctNumbersArray.join(' - '), 'Correct numbers: ' + correctNumbersArray.length);
+
+    console.log(wrongNumbersArray.join(' - '), 'Wrong numbers: ' + wrongNumbersArray.length);
+}
